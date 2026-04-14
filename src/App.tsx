f57b08328eb54a8d19abfc28b9d24e8791d4fd12
emilenrique92 <macxi.quikin@gmail.com>
2026-04-14 09:50:31 +0200

    Delete src/App.tsx

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Fast Portuguese 🇧🇷</h1>
-      <p>La app está funcionando correctamente 🚀</p>
-    </div>
-  );
-}
